8447cbb4b180c23f7aadafcbbcf3a75029e3fa91
Elizabeth Mendoza <emm797@nyu.edu>
2021-11-21 23:40:32 -0500

    homework 9 revised

--- Homeworks/mediumJava.js
+++ Homeworks/mediumJava.js
@@ -1,16 +1,9 @@
-console.log("theMessage is here! (the javascript is here)")
 
 const theButton = document.getElementById("firstButton");
-let theBody = document.querySelector("body");
-let thePara = document.querySelector("p");
-// console.log(thePara)
 
 theButton.addEventListener('click', makeDo);
 
 function makeDo(){
-    console.log("You clicked the button!");
-    theBody.style.backgroundColor = "green";
-    theBody.style.color = "white"
     location.href = newLink;
 
 }
