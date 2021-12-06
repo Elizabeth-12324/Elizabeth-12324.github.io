dc006d20273d6a7a0328052de9e9ca018c40c1e8
Elizabeth Mendoza <emm797@nyu.edu>
2021-12-06 03:41:17 -0500

    button

--- FinalProject/finalJava.js
+++ FinalProject/finalJava.js
@@ -1,42 +1,35 @@
-//¿Que me ves? Largo, shoo!
-var pasito = 0;
-var nomeveas = "MidtermPage2.html"
-//var bipbip = new Audio('images/hmm.mp3');
-var bipbip = new Audio('chime.mp3');
-//var ufail = new Audio('images/err.mp3');
-var ufail=new Audio('error.mp3');
-function puzzle(orden) {
-	if (orden == ++puzzle){
-		puzzle = orden;
-		bipbip.play();
-		//alert("Ok! " + orden);
-	} else {
-		//alert("Err!");
-		puzzle = 0;
-		ufail.play();
-	}
-	if(puzzle ==3){
-		location.href = nomeveas;
-	}
+let theBody = document.querySelector("body");
+let thePara = document.querySelector("p");
+
+var audio1 = new Audio("backgroundMP3");
+var audio2 = new Audio("crash");
+
+
+
+// console.log(thePara)
+
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+
+
+
+
+button1.addEventListener('click', increment);
+button2.addEventListener('click', increment);
+
+let incrementor = 1;
+function increment(){
+
+    console.log("You clicked the button!");
+    if (incrementor == 1) {
+      button1.style.display = "block";
+      button2.style.display = "block";
+
+    } else if (incrementor == 2) {
+      button2.style.display = "none";
+      button1.style.display = "none";
+
+  }
+    incrementor++;
+
 }
-
-function fadeIn() {
-	$('.fade-in').each(function() {
-			var top_of_element = $(this).offset().top;
-			var bottom_of_element = $(this).offset().top + $(this).outerHeight();
-			var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
-			var top_of_screen = $(window).scrollTop();
-
-			if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element) && !$(this).hasClass('is-visible')) {
-					$(this).addClass('is-visible');
-			}
-	});
-}
-
-$(function() {
-		fadeIn();
-});
-
-$(window).scroll(function() {
-		fadeIn();
-});
