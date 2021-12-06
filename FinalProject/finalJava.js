7801112fb7528b755c9a190605607c5660fc737f
Elizabeth Mendoza <emm797@nyu.edu>
2021-12-06 04:08:44 -0500

    intro cutscene

--- FinalProject/finalJava.js
+++ FinalProject/finalJava.js
@@ -10,13 +10,14 @@ var audio2 = new Audio("crash");
 
 const button1 = document.getElementById("button1");
 const button2 = document.getElementById("button2");
+const button3=document.getElementById("button3");
 
 
 
 
 button1.addEventListener('click', increment);
 button2.addEventListener('click', increment);
-
+button3.addEventListener('click',increment);
 let incrementor = 1;
 function increment(){
 
@@ -28,8 +29,15 @@ function increment(){
     } else if (incrementor == 2) {
       button2.style.display = "none";
       button1.style.display = "none";
+			button3.style.display="block";
 
   }
+ else if (incrementor == 3) {
+	button2.style.display = "none";
+	button1.style.display = "none";
+	button3.style.display="none";
+
+}
     incrementor++;
 
 }
